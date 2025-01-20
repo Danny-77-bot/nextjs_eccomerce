7a04e1622e2b07d4322ccd016ef8e576c147e06c
Daniel <danielmelaku1177@gmail.com>
2025-01-20 22:11:38 +0300

    creaed best selling slider

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -13,6 +13,7 @@ export default async function HomePage() {
    
   const categories = (await getAllCategories()).slice(0, 4)
   const todaysDeals = await getProductsByTag({ tag: 'todays-deal' })
+  const bestSellingProducts = await getProductsByTag({ tag: 'best-seller' })
 
     
   const newArrivals = await getProductsForCard({
@@ -27,6 +28,7 @@ export default async function HomePage() {
     tag: 'best-seller',
     limit: 4,
   })
+
   const cards = [
     {
       title: 'Categories to explore',
@@ -76,6 +78,15 @@ export default async function HomePage() {
           <ProductSlider title={"Today's Deals"} products={todaysDeals} />
         </CardContent>
       </Card>
+       <Card className='w-full rounded-none'>
+    <CardContent className='p-4 items-center gap-3'>
+      <ProductSlider
+        title='Best Selling Products'
+        products={bestSellingProducts}
+        hideDetails
+      />
+    </CardContent>
+  </Card>
     </div>
   </>
   )
